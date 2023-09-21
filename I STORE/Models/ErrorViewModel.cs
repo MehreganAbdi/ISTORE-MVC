d85dd28b16dda005049ba8f15282deaa9e50305c
Mehregan Abdi <mehreganabdix@gmail.com>
2023-09-21 20:48:59 +0330

    Add project files.

---
+++ I STORE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace I_STORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
