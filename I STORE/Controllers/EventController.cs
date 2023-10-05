9cbc1da900583c76bb678f047210d846419bcb92
Mehregan Abdi <mehreganabdix@gmail.com>
2023-10-05 10:13:15 +0330

    Create And Edit Event Has Some Problem

--- I STORE/Controllers/EventController.cs
+++ I STORE/Controllers/EventController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Interfaces;
+using Context.Models;
+using Context.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace I_STORE.Controllers
 {
     public class EventController : Controller
     {
+        private readonly IEventRepository _eventRepository;
+        public EventController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var events = await _eventRepository.GetAllAsync();
+            return View(events);
+        }
         public async Task<IActionResult> Create()
         {
-            return View();
+            var eventt = new Event();
+            return View(eventt);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Event eventt)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(eventt);
+            }
+            await _eventRepository.AddAsync(eventt);
+            return RedirectToAction("Index", "Event");
+        }
+
+        public async Task<IActionResult> PSDetail(int Id)
+        {
+            var model = new PSDetailVM();
+            var eventt = await _eventRepository.GetByIdAsync(Id);
+            var products = eventt.ProductsEvents;
+            var sneakers = eventt.SneakerEvents;
+            if (products != null)
+            {
+                foreach (var item in products)
+                {
+                    model.Products.Add(item);
+                }
+            }
+            if(sneakers != null)
+            {
+                foreach (var item in sneakers)
+                {
+                    model.Sneakers.Add(item);
+                }
+            }
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            _eventRepository.Remove(await _eventRepository.GetByIdAsync(Id));
+            return RedirectToAction("Index", "Event");
         }
     }
 }
